9a8b8a52c9b9f8ef07d7264e411df5de0d735e17
RiteshDholkhediya <riteshkheda444@gmail.com>
2024-03-19 10:38:32 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -17,4 +17,10 @@ app.use(express.static("public")); // this middleware is used to handle images, 
 
 app.use(cookieParser()); // it is for implementing CRUD operation to the client browser()
 
+// routes imports
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter); // Whenever you are using Router you need to use middleware this will pass the controll to the userRouter file (./routes/user.routes.js) then whaterver you will do it will work
+// https://localhost:8000/api/v1/users/
 export { app };
