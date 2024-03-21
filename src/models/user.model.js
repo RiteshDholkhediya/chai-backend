14c8953b37a9ffcddf20074d9324e4c9381ea25e
RiteshDholkhediya <riteshkheda444@gmail.com>
2024-03-21 13:37:39 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 const userSchema = new Schema(
@@ -35,7 +35,7 @@ const userSchema = new Schema(
 
     watchHistory: [
       {
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId, // here could be error
         ref: "Video",
       },
     ],
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
